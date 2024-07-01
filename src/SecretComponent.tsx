db308b3b430fc8788b5fec446df440189fa619f0
muhammadhamzakhann <muhammad.khan@trustedshops.de>
2024-07-01 03:56:20 +0200

    trigger workflow

--- src/SecretComponent.tsx
+++ src/SecretComponent.tsx
@@ -8,18 +8,24 @@ const SecretComponent: React.FC = () => {
     const fetchSecret = async () => {
       const secretName = 'secretmaangertest';  // Change this to your secret name
       const region = 'eu-central-1';  // Change this to your AWS region
-
+      const secretKey = 'REACT_APP_API_KEY';  // Change this to the specific key you want to retrieve
       AWS.config.update({ region: region });
 
-      const client = new AWS.SecretsManager({ region: region });
+      const client = new AWS.SecretsManager();
 
       try {
         const data = await client.getSecretValue({ SecretId: secretName }).promise();
+        let secretString;
         if ('SecretString' in data) {
-          setSecretValue(data.SecretString!);
+          secretString = data.SecretString;
         } else {
           const buff = Buffer.from(data.SecretBinary as string, 'base64');
-          setSecretValue(buff.toString('ascii'));
+          secretString = buff.toString('ascii');
+        }
+
+        if (secretString) {
+          const secretJSON = JSON.parse(secretString);
+          setSecretValue(secretJSON[secretKey]);
         }
       } catch (err) {
         console.error('Error fetching secret:', err);
