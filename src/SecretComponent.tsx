babe072b33bd27275891492c01bd2239ccbb0417
muhammadhamzakhann <muhammad.khan@trustedshops.de>
2024-07-01 12:09:24 +0200

    trigger workflow

--- src/SecretComponent.tsx
+++ src/SecretComponent.tsx
@@ -1,35 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
-
-const secret_name = "secretmaangertest";
-
-const client = new SecretsManagerClient({
-  region: "eu-central-1",
-});
 
 const SecretComponent: React.FC = () => {
   const [secret, setSecret] = useState<Record<string, string> | null>(null);
-  const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchSecret = async () => {
-      let response;
       try {
-        response = await client.send(
-          new GetSecretValueCommand({
-            SecretId: secret_name,
-            VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-          })
-        );
-        if (response.SecretString) {
-          const secretJson = JSON.parse(response.SecretString);
-          setSecret(secretJson);
+        const response = await fetch('https://73afw5og0k.execute-api.eu-central-1.amazonaws.com/default/secrets');
+        if (!response.ok) {
+          throw new Error('Failed to fetch secret');
         }
+        const data = await response.json();
+        const secretJson = JSON.parse(data.secret);
+        setSecret(secretJson);
       } catch (err) {
-        setError(err as Error);
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError('An unknown error occurred');
+        }
       }
     };
 
@@ -37,17 +27,15 @@ const SecretComponent: React.FC = () => {
   }, []);
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div>Error: {error}</div>;
   }
 
   return (
     <div>
       {secret ? (
         <div>
-          {/* Access specific values from the secret JSON object */}
           <div>Key1: {secret.API_SECRET}</div>
-          <div>Key2: {secret.API_KEY}</div>
-          {/* Add more keys as needed */}
+          <div>Key2: {secret.API_ACCESS}</div>
         </div>
       ) : (
         <div>Loading...</div>
