e19c577e7fc7bf8f465650335b710a5e824e9d91
muhammadhamzakhann <muhammad.khan@trustedshops.de>
2024-07-01 05:14:46 +0200

    trigger workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import SecretComponent from './SecretComponent';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to Secrets Manager Demo</h1>
         <SecretComponent />
       </header>
     </div>
