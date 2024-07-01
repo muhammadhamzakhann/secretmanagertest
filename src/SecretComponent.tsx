50c0fdca6f270dd2d20848880988a1b6c2a461c1
muhammadhamzakhann <muhammad.khan@trustedshops.de>
2024-07-01 04:13:24 +0200

    trigger workflow

--- src/SecretComponent.tsx
+++ src/SecretComponent.tsx
@@ -1,44 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import AWS from 'aws-sdk';
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const secret_name = "secretmaangertest";
+
+const client = new SecretsManagerClient({
+  region: "eu-central-1",
+});
 
 const SecretComponent: React.FC = () => {
-  const [secretValue, setSecretValue] = useState<string>('');
+  const [secret, setSecret] = useState<string | null>(null);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     const fetchSecret = async () => {
-      const secretName = 'secretmaangertest';  // Change this to your secret name
-      const region = 'eu-central-1';  // Change this to your AWS region
-      const secretKey = 'REACT_APP_API_KEY';  // Change this to the specific key you want to retrieve
-      AWS.config.update({ region: region });
-
-      const client = new AWS.SecretsManager();
-
+      let response;
       try {
-        const data = await client.getSecretValue({ SecretId: secretName }).promise();
-        let secretString;
-        if ('SecretString' in data) {
-          secretString = data.SecretString;
-        } else {
-          const buff = Buffer.from(data.SecretBinary as string, 'base64');
-          secretString = buff.toString('ascii');
-        }
-
-        if (secretString) {
-          const secretJSON = JSON.parse(secretString);
-          setSecretValue(secretJSON[secretKey]);
-        }
+        response = await client.send(
+          new GetSecretValueCommand({
+            SecretId: secret_name,
+            VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+          })
+        );
+        setSecret(response.SecretString || null);
       } catch (err) {
-        console.error('Error fetching secret:', err);
+        setError(err as Error);
       }
     };
 
     fetchSecret();
   }, []);
 
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div>
-      <h1>Secret Value:</h1>
-      <p>{secretValue}</p>
+      {secret ? <div>Secret: {secret}</div> : <div>Loading...</div>}
     </div>
   );
 };
